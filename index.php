69454f7bc3038ac698121bc324307f161f683257
Diego Prates <prates.diego@gmail.com>
2025-09-04 16:07:34 -0300

    Feat: Codificação HTML do portal.

---
+++ index.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--SEO-->
+    <meta name="description" content="Site dinâmico com gerenciamento através de painel de Controle em PHP">
+    <meta name="keywords" content="Projeto Web, CMS, PHP, HTML">
+    <meta name="author" content="Diego Prates">
+
+    <!--FontAwesome-->
+    <script src="https://kit.fontawesome.com/ad7ef30e1e.js" crossorigin="anonymous"></script>
+
+    <title>ProjetoWeb</title>
+
+</head>
+
+<body>
+
+    <header>
+        <div class="container">
+
+            <div class="logo">Logomarca</div>
+
+            <nav class="desktop">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav><!--desktop-->
+
+            <nav class="mobile">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav><!--mobile-->
+
+        </div><!--container-->
+
+    </header>
+
+    <section class="banner-principal">
+
+        <div class="container">
+
+            <form>
+                <h2>Qual o seu melhor e-mail?</h2>
+                <input type="email" name="email" required>
+                <input type="submit" value="Cadastrar!">
+            </form>
+
+        </div><!--container-->
+
+    </section><!--banner-principal-->
+
+    <section class="descricao-autor">
+
+        <div class="container">
+
+            <div class="w50">
+                <h2>Lorem Ipsum</h2>
+                <p>
+                    Tempor consectetur adipisicing exercitation eu aliqua qui incididunt et ex mollit magna sint. Sint
+                    elit ad et laboris eiusmod dolor est aliqua. Laborum est pariatur eu ex ea nisi voluptate qui
+                    consectetur eu non Lorem.
+                </p>
+                <p>
+                    Elit enim sint ex amet aliquip culpa do ea enim. Id velit cupidatat excepteur eiusmod ipsum duis
+                    laboris et sit consequat sint et. Ipsum officia consequat anim minim anim aute nostrud culpa tempor
+                    amet excepteur. Elit non reprehenderit sint anim sunt laboris excepteur. Incididunt Lorem in
+                    occaecat nisi.
+                </p>
+            </div><!--w50-->
+
+            <div class="w50">
+                <img src="images/foto.jpg" alt="Fotografia do autor do site.">
+            </div><!--w50-->
+
+        </div><!--container-->
+
+    </section><!--descricao-autor-->
+
+    <section class="especialidades">
+
+        <div class="container">
+
+            <h2 class="title">Especialidades</h2>
+
+            <div class="box-especialidades">
+                <h3>Icone 01</h3>
+                <h3>CSS</h3>
+                <p>Labore consectetur ipsum incididunt excepteur irure reprehenderit. Ipsum laboris magna do laboris
+                    elit Lorem. Non consectetur enim ex mollit nulla in ea adipisicing reprehenderit eu id do minim
+                    velit.</p>
+            </div><!--box-especialidades-->
+
+            <div class="box-especialidades">
+                <h3>Icone 02</h3>
+                <h3>HTML2</h3>
+                <p>Labore consectetur ipsum incididunt excepteur irure reprehenderit. Ipsum laboris magna do laboris
+                    elit Lorem. Non consectetur enim ex mollit nulla in ea adipisicing reprehenderit eu id do minim
+                    velit.</p>
+            </div><!--box-especialidades-->
+
+            <div class="box-especialidades">
+                <h3>Icone 03</h3>
+                <h3>JavaScript</h3>
+                <p>Labore consectetur ipsum incididunt excepteur irure reprehenderit. Ipsum laboris magna do laboris
+                    elit Lorem. Non consectetur enim ex mollit nulla in ea adipisicing reprehenderit eu id do minim
+                    velit.</p>
+            </div><!--box-especialidades-->
+
+        </div><!--container-->
+
+    </section><!--especialidades-->
+
+    <section class="extras">
+
+        <div class="container">
+
+            <div class="w50">
+
+                <h2 class="title">Depoimentos dos nossos clientes</h2>
+
+                <div class="depoimento-single">
+                    <p class="depoimento-descricao">Aliqua duis voluptate nulla enim eu dolore et sunt irure cupidatat
+                        aliqua Lorem proident ad. Occaecat velit et quis reprehenderit. Quis elit tempor do
+                        reprehenderit excepteur. Minim ad aute nulla sit nisi incididunt consequat ipsum labore. Culpa
+                        exercitation aliquip dolore non dolore voluptate fugiat enim. Reprehenderit proident id aute
+                        magna officia voluptate ea elit excepteur nostrud.</p>
+                    <p class="nome-autor">Lorem Ipsum</p>
+                </div><!--depoimento-single-->
+
+                <div class="depoimento-single">
+                    <p class="depoimento-descricao">Aliqua duis voluptate nulla enim eu dolore et sunt irure cupidatat
+                        aliqua Lorem proident ad. Occaecat velit et quis reprehenderit. Quis elit tempor do
+                        reprehenderit excepteur. Minim ad aute nulla sit nisi incididunt consequat ipsum labore. Culpa
+                        exercitation aliquip dolore non dolore voluptate fugiat enim. Reprehenderit proident id aute
+                        magna officia voluptate ea elit excepteur nostrud.</p>
+                    <p class="nome-autor">Lorem Ipsum</p>
+                </div><!--depoimento-single-->
+
+                <div class="depoimento-single">
+                    <p class="depoimento-descricao">Aliqua duis voluptate nulla enim eu dolore et sunt irure cupidatat
+                        aliqua Lorem proident ad. Occaecat velit et quis reprehenderit. Quis elit tempor do
+                        reprehenderit excepteur. Minim ad aute nulla sit nisi incididunt consequat ipsum labore. Culpa
+                        exercitation aliquip dolore non dolore voluptate fugiat enim. Reprehenderit proident id aute
+                        magna officia voluptate ea elit excepteur nostrud.</p>
+                    <p class="nome-autor">Lorem Ipsum</p>
+                </div><!--depoimento-single-->
+
+            </div><!--w50-->
+
+            <div class="w50">
+                <h2 class="title">Serviços</h2>
+
+                <div class="servicos">
+                    <ul>
+                        <li>Aliqua duis voluptate nulla enim eu dolore et sunt irure cupidatat
+                            aliqua Lorem proident ad.</li>
+                        <li>Aliqua duis voluptate nulla enim eu dolore et sunt irure cupidatat
+                            aliqua Lorem proident ad.</li>
+                        <li>Aliqua duis voluptate nulla enim eu dolore et sunt irure cupidatat
+                            aliqua Lorem proident ad.</li>
+                    </ul>
+                </div><!--servicos-->
+
+            </div><!--w50-->
+
+        </div><!--container-->
+
+    </section><!--extras-->
+
+    <footer>
+
+        <div class="container">
+            <p>Todos os direitos reservados</p>
+        </div><!--container-->
+
+    </footer>
+
+</body>
+
+</html>
